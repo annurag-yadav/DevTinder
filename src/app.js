d8b6ec5b3dc905f009d826f4c2a3c06644ebb26b
Anurag Yadav <itsanuragrao@gmail.com>
2026-04-28 17:34:36 +0530

    init-database

--- src/app.js
+++ src/app.js
@@ -1,10 +1,39 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const connectDB = require('./config/database');
+const User = require('./models/user');
 
-app.use("/test",(req,res) => {
-  res.send('Helllo World!'); });     
+app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.post("/signup", async(req,res) =>{
+   const user = new User({
+    firstName : req.body.firstName,
+    lastName : req.body.lastName,
+    emailId : req.body.emailId,
+    password : req.body.password,
+    age : req.body.age,
+    gender : req.body.gender
+   })
+   try { await user.save()
+
+    res.status(200).json({message : "User created successfully"})
+   } catch (error) {
+    res.status(500).json({message : "Error creating user", error : error.message})
+   }
+
+})
+
+
+
+connectDB()
+  .then(() =>{
+    console.log('Database connected successfully');
+    app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
+  })
+  .catch((error) => {
+    console.error('Error connecting to database:', error);
+  });
+
